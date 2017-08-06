0deb04c6a7e0e01c247863d5a234df73b7dbd9cb
wuwentao <wuwentao@qiyi.com>
2017-08-06 12:18:44 +0800

    Init project

---
+++ Loves/WWTFrame.h
@@ -0,0 +1,13 @@
+//
+//  WWTFrame.h
+//  WWTFrame
+//
+//  Created by wuwentao on 17/8/5.
+//  Copyright © 2017年 wuwentao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WWTFrame : NSObject
+- (void)printTest;
+@end
---
+++ WWTFrame/WWTFrame.m
@@ -0,0 +1,15 @@
+//
+//  WWTFrame.m
+//  WWTFrame
+//
+//  Created by wuwentao on 17/8/5.
+//  Copyright © 2017年 wuwentao. All rights reserved.
+//
+
+#import "WWTFrame.h"
+
+@implementation WWTFrame
+- (void)printTest{
+    NSLog(@"this is a test");
+}
+@end
